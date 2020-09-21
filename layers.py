Memory optimizations, fix NaN gradients from std, improve progressbar

layers.py
@@ -318,6 +318,8 @@ class Project3D(nn.Module):
 
         self.ones = nn.Parameter(torch.ones(self.batch_size, 1, self.height * self.width),
                                  requires_grad=False)
+        self.zeros = nn.Parameter(torch.zeros(size=(self.batch_size // 6, 1, self.height * self.width), dtype=torch.float),
+                                  requires_grad=False)
 
     def forward(self, points, K, T):
         P = T[:, :3, :]
@@ -352,7 +354,7 @@ class Project3D(nn.Module):
                 # 1 -> +X -> Right, -1 -> -X -> Left, 2 -> Y -> Bottom, -2 -> -Y -> Top, 3 -> Z -> Front, -3 -> -Z -> Back
                 # side -> mags value
                 indices = {Side.Top: -2, Side.Bottom: 2, Side.Left: -1, Side.Right: 1, Side.Front: 3, Side.Back: -3}
-                sides = torch.zeros(size=(self.batch_size // 6, 1, self.height * self.width), dtype=torch.float).to(points.device) + side.value
+                sides = self.zeros + side.value
 
                 split_world_coords = split_coords(side_world_coords)
 

networks/cube_poses.py
@@ -111,7 +111,9 @@ class CubePosesAndLoss(nn.Module):
         pose = sides_to_batch(top, bottom, left, right, front, back)
 
         if self.include_loss:
-            loss = poses.std(dim=1, keepdim=False, unbiased=False).sum(1).sum(1)
+            #TODO check unbiased.  Unbiased = devide by N, biased = devide by n-1
+            #TODO using var instead of std cause std causes NaN in gradients
+            loss = poses.var(dim=1, keepdim=False, unbiased=False).sum(1).sum(1)
             return loss, pose
         else:
             return pose

trainer.py
@@ -221,7 +221,7 @@ class Trainer:
         self.epoch = 0
         self.step = 0
         self.start_time = time.time()
-        for self.epoch in trange(self.opt.num_epochs, desc="Epochs"):
+        for self.epoch in trange(self.opt.num_epochs, desc="Epochs", ncols=200):
             self.run_epoch()
             if (self.epoch + 1) % self.opt.save_frequency == 0:
                 self.save_model()
@@ -233,22 +233,24 @@ class Trainer:
         # print("Training Epoch", self.step)
         self.set_train()
 
-        pbar = tqdm(total=self.train_items, desc="Batches")
+        pbar = tqdm(total=self.train_items, desc="Batches", ncols=200)
 
         for batch_idx, inputs in enumerate(self.train_loader):
             if self.opt.mode is Mode.Cubemap:
                 inputs = convert_to_cubemap_batch(inputs, self.opt.frame_ids, self.opt.scales)
 
+            self.model_optimizer.zero_grad()
             before_op_time = time.time()
 
+            # with torch.autograd.detect_anomaly():
             outputs, losses = self.process_batch(inputs)
 
-            self.model_optimizer.zero_grad()
             losses["loss"].backward()
             self.model_optimizer.step()
             self.model_lr_scheduler.step()
 
             duration = time.time() - before_op_time
+            pbar.set_postfix_str(f"Epoch {self.epoch}, Loss: {losses['loss'].cpu().data:.6f}", refresh=True)
 
             # log less frequently after the first 2000 steps to save time & disk space
             early_phase = batch_idx % self.opt.log_frequency == 0 and self.step < 2000
@@ -438,7 +440,7 @@ class Trainer:
                 pix_coords = self.project_3d[source_scale](
                     cam_points, inputs[("K", source_scale)], T)
 
-                outputs[("sample", frame_id, scale)] = pix_coords
+                # outputs[("sample", frame_id, scale)] = pix_coords
 
                 if self.opt.mode is Mode.Cubemap:
                     color = inputs[("color", frame_id, source_scale)]